8a959327d4620754879365fc096b96d7e2631b9b
Mathias Gerber <mat.gerber@gmx.ch>
2019-06-18 13:40:52 +0200

    conf changed for read the docs

--- Pythontraining/docs/conf.py
+++ Pythontraining/docs/conf.py
@@ -43,6 +43,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 autodoc_mock_imports = ["adventuregame"]
 
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
